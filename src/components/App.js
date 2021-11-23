0fee8b6fa2b31058abaebd450a5afc401bc95fb7
Hilda Diana Cardenas <hcardenas00@gmail.com>
2021-11-23 13:08:59 -0500

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,24 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
+// console.log("user:", user.name)
+
 
 function App() {
   return (
-    <div>
+    <div>     
       <NavBar />
-      <Home />
-      <About />
+
+      <Home 
+        username={user.name}
+        city={user.city}
+        color={user.color}
+        />
+
+      <About
+        bio={user.bio}
+      />
+
     </div>
   );
 }
